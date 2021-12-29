c3fe4344eeb8c6baf4a3688f6ad95de2ad986a7e
Rafael Guedes <rafaelguedes.ufam@gmail.com>
2021-12-29 08:53:16 -0400

    dev(#3): Inserção de NavBar padrão Instalação dos módulos do Bootstrap

--- acolhe-mais/src/components/layout/Navbar.js
+++ acolhe-mais/src/components/layout/Navbar.js
@@ -2,32 +2,35 @@ import { Link } from "react-router-dom"
 import Container from "./Container"
 import styles from './Navbar.module.css'
 import logo from '../img/AcolheIcon-150x150.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar (){
     return(
-       
-        <nav>
-           
-                <Link to ="/">
-                    <img src={logo} alt ="Acolhe+"/>
-                </Link>
-               
-           <ul className={styles.list}>
-               <li className={styles.item}>
-               <Link to ="/">Home</Link>
-               </li>
-               <li className={styles.item}>
-               <Link to ="/plataforma">Plataformas</Link>
-               </li>
-               <li className={styles.item}>
-               <Link to ="/sobre">Sobre</Link>
-              
-               </li>
-              
-           </ul>
-          
-        </nav>
-      
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container border-bottom">
+                    <Link class="navbar-brand" to="/">
+                        <img src={logo} alt="Acolhe+ Logo"  />
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto">
+                            <li className={styles.item}>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link to="/plataforma">Plataformas</Link>
+                            </li>
+                            <li className={styles.item}>
+                                <Link to="/sobre">Sobre</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 }
 
